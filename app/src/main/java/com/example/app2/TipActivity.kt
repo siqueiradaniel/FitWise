5a6d561cc2e26483986320dfd3efca60283675d6
siqueiradaniel <danielsiqueira111@gmail.com>
2025-01-07 11:20:15 -0300

    feat: doesn't change the tip when switch Icon

--- app/src/main/java/com/example/app2/TipActivity.kt
+++ app/src/main/java/com/example/app2/TipActivity.kt
@@ -14,6 +14,8 @@ class TipActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var myPreferences: MyPreferences
     private var randomTip: RandomTip = RandomTip()
     private var isNutritionShown: Boolean = true
+    private var nutritionTip = randomTip.randomNutritionTip()
+    private var musculationTip = randomTip.randomWorkoutTip()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +23,12 @@ class TipActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         myPreferences = MyPreferences(this)
-
         val username = myPreferences.getString("username")
         binding.tipGreeting.text = "${binding.tipGreeting.text} $username"
 
-        val nutritionTip = randomTip.randomNutritionTip()
         binding.tipGeneratedText.text = nutritionTip
-
         binding.tipNutritionIcon.drawable.setTint(ContextCompat.getColor(this, R.color.tip_stroke))
+
         binding.tipButtonGenerate.setOnClickListener(this)
         binding.tipNutritionIcon.setOnClickListener(this)
         binding.tipMusculationIcon.setOnClickListener(this)
@@ -38,10 +38,14 @@ class TipActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if(view.id == R.id.tipButtonGenerate) {
-            val tip = if(isNutritionShown) {
-                randomTip.randomNutritionTip()
+            val tip: String
+
+            if(isNutritionShown) {
+                tip = randomTip.randomNutritionTip()
+                nutritionTip = tip
             } else {
-                randomTip.randomWorkoutTip()
+                tip = randomTip.randomWorkoutTip()
+                musculationTip = tip
             }
             binding.tipGeneratedText.text = tip
         } else if (view.id == R.id.tipNutritionIcon) {
@@ -49,14 +53,14 @@ class TipActivity : AppCompatActivity(), View.OnClickListener {
                 binding.tipMusculationIcon.drawable.setTint(ContextCompat.getColor(this, R.color.white))
                 binding.tipNutritionIcon.drawable.setTint(ContextCompat.getColor(this, R.color.tip_stroke))
                 isNutritionShown = true
-                binding.tipGeneratedText.text = randomTip.randomNutritionTip()
+                binding.tipGeneratedText.text = nutritionTip
             }
         } else if (view.id == R.id.tipMusculationIcon) {
             if (isNutritionShown) {
                 binding.tipMusculationIcon.drawable.setTint(ContextCompat.getColor(this, R.color.tip_stroke))
                 binding.tipNutritionIcon.drawable.setTint(ContextCompat.getColor(this, R.color.white))
                 isNutritionShown = false
-                binding.tipGeneratedText.text = randomTip.randomWorkoutTip()
+                binding.tipGeneratedText.text = musculationTip
             }
         }
     }
